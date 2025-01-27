b040256c53663e5d48f66374d2c3921ad68791d8
Emirkan <Emirkanoktay12@gmail.com>
2025-01-27 17:18:10 +0300

    Caresoual Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import CaresoualHeader from './Compenents/CaresoualHeader'
 import Header from './Pages/Header'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <div>
       <Header />
+      <CaresoualHeader />
     </div>
   )
 }
---
+++ src/Compenents/CaresoualHeader.tsx
@@ -0,0 +1,28 @@
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+import Carousel1 from '../images/careousal1.webp'
+import Carousel2 from '../images/careousal2.webp'
+import '../Css/CaresoualHeader.css'
+
+function CaresoualHeader() {
+    return (
+        <Carousel className="Carousel"
+            showThumbs={false}
+            showArrows={false}
+            showStatus={false}
+            infiniteLoop={true}
+            autoPlay={true}
+            showIndicators={false}
+            interval={2500}>
+            <div className="images-div">
+                <img src={Carousel1} alt="" />
+            </div>
+            <div>
+                <img src={Carousel2} />
+            </div>
+        </Carousel>
+
+    )
+}
+
+export default CaresoualHeader
